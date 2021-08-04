6672cd9d4a3a1c1d4b8290fa67ecce6a83eedfdc
LAIBA-IRFAN <66326254+LAIBA-IRFAN@users.noreply.github.com>
2021-08-04 09:39:48 -0700

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'I21xauur7t');
+define('DB_PASSWORD', 'GaABKRicdV');
+define('DB_NAME', 'I21xauur7t');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
